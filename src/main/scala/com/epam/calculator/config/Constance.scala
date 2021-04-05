37f88a63770814264293ed3738fc0184df71b7bc
anastasiya315510 <52715819+anastasiya315510@users.noreply.github.com>
2021-04-05 12:37:17 +0300

    Update Constance.scala

--- src/main/scala/com/epam/calculator/config/Constance.scala
+++ src/main/scala/com/epam/calculator/config/Constance.scala
@@ -6,7 +6,7 @@
 package com.epam.calculator.config
 
 object Constance {
-  implicit val key = "AIzaSyBtNqV1oiOjOO92nkob0z4ytOaxzp3hRdw"
+  implicit val key = "your key"
   implicit val googleGetCoordinatesURI ="https://maps.googleapis.com/maps/api/geocode/json"
   implicit val URI = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="
 
